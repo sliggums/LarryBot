4b648d440da5bb4c1e1937b4685546ccb17197fc
Steve Wang <s.w@berkeley.edu>
2016-07-13 21:19:38 -0700

    Fixed errors

--- requestbot.py
+++ requestbot.py
@@ -16,13 +16,13 @@ auth = OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 
 class LarryError(Exception):
     def __init__(self, error_msg):
-        self.error_msg = msg
+        self.error_msg = error_msg
     def __str__(self):
         return repr(self.error_msg)
 
 class LarryBot:
 
-    BAG_OF_WORDS = ["RT giveaway", "RT #win", "RT chance win", "RT giving away", "RT to enter"]
+    BAG_OF_WORDS = ["chance win", "giving away"]
     BANNED_USERS = ['competition', "bot", "safarics", "csgo", "fut", "pokemon"]
     BANNED_WORDS = ["rt @", "csgo", "dragon", "rose", "blackops", "pokemon", "cod", "header", "baby", "madden", "fifa", "funko", "uk"]
     MIN_FOLLOWERS = 1000
@@ -46,32 +46,40 @@ class LarryBot:
         update_url = "%sstatuses/update.json" % self.url 
         update_params = {'status':status}
         r = self.session.post(update_url, params=update_params)
-        errors = r.json()['errors']
-        if errors:
-             raise errors[0]['message']
+        try:
+            error = r.json()['errors']
+            raise LarryError(error)
+        except:
+            pass
 
     def retweet(self, tweet_id):
         retweet_url = "%sstatuses/retweet/%s.json" % (self.url, tweet_id)
         r = self.session.post(retweet_url)
-        errors = r.json()['errors']
-        if errors:
-             raise LarryError(errors[0]['message'])
+        try:
+            error = r.json()['errors']
+            raise LarryError(error)
+        except:
+            pass
 
     def like(self, tweet_id):
         like_url = "%sfavorites/create.json" % self.url
         like_params = {'id':tweet_id}
         r = self.session.post(like_url, params=like_params)
-        errors = r.json()['errors']
-        if errors:
-             print errors[0]['message']
+        try:
+            error = r.json()['errors']
+            print error
+        except:
+            pass
 
     def add_friend(self, user_id):
         add_friend_url = "%sfriendships/create.json" % self.url
         add_friend_params = {'id':user_id}
         r = self.session.post(add_friend_url, params=add_friend_params)
-        errors = r.json()['errors']
-        if errors:
-             print errors[0]['message']
+        try:
+            error = r.json()['errors']
+            print error
+        except:
+            pass
 
     def find_status(self, tweet_id):
         find_url = "%sstatuses/show.json" % self.url
@@ -91,6 +99,7 @@ class LarryBot:
                 try:
                     r = self.search(words)
                     for data in r['statuses']:
+                        print data['text']
                         in_set, tweet_id = self.get_id(data)
                         if not in_set:
                             self.tweet(tweet_id)
@@ -107,7 +116,6 @@ class LarryBot:
             correct_tweet = data['retweeted_status']
         except KeyError:
             correct_tweet = data
-
         correct_id = correct_tweet['id']
         in_set = correct_id in self.list_of_tweets
         self.list_of_tweets.add(correct_id)
@@ -118,35 +126,37 @@ class LarryBot:
 
         # Make sure user is of some status and is not reposting something.
         curr_followers = correct_tweet['user']['followers_count']
-        try: 
-            if curr_followers < self.MIN_FOLLOWERS or correct_tweet['entities']['urls']:
+        print "curr_followers is: " + str(curr_followers)
+        if curr_followers < self.MIN_FOLLOWERS or correct_tweet['entities']['urls']:
+            print "is a retweet or not enough followers"
+            return
+
+        # Check whether it's something we don't want, which we specify at the top.
+        screen_name = correct_tweet['user']['screen_name']
+        given_name = correct_tweet['user']['name']
+        tweet = correct_tweet['text']
+        tweet_lower = tweet.lower()
+
+        if "rt" not in tweet_lower and "retweet" not in tweet_lower:
+            print "not a giveaway"
+            return
+        for word in self.BANNED_WORDS:
+            if word in tweet_lower:
+                return
+        user_lower = screen_name.lower()
+        given_lower = given_name.lower()
+        for user in self.BANNED_USERS:
+            if user in user_lower or user in given_lower:
                 return
 
-            # Check whether it's something we don't want, which we specify at the top.
-            screen_name = correct_tweet['user']['screen_name']
-            given_name = correct_tweet['user']['name']
-            tweet = correct_tweet['text']
-            tweet_lower = tweet.lower()
+        day_to_delete = (datetime.datetime.today() + datetime.timedelta(days=7)).day
+        f = open(str(day_to_delete) + ".csv", "a")
+        writer = csv.writer(f)
 
-            if not ("rt" in tweet_lower or "retweet" in tweet_lower):
-                return
-            for word in self.BANNED_WORDS:
-                if word in tweet_lower:
-                    return
-            user_lower = screen_name.lower()
-            given_lower = given_name.lower()
-            for user in self.BANNED_USERS:
-                if user in user_lower or user in given_lower:
-                    return
-
-            day_to_delete = (datetime.datetime.today() + datetime.timedelta(days=7)).day
-            f = open(str(day_to_delete) + ".csv", "a")
-            writer = csv.writer(f)
-
-            # Check that this isn't an actual retweet.
-            others = correct_tweet['entities']['user_mentions']
-            if not others:
-                pass
+        # Check that this isn't an actual retweet.
+        others = correct_tweet['entities']['user_mentions']
+        print others
+        if others:
             for person in others:
                 other_id = person['id']
                 other_followers = self.find_user(other_id)['followers_count']
@@ -159,22 +169,21 @@ class LarryBot:
                         writer.writerow([other_id])
                     else:
                         return
-        except KeyError:
-            pass
 
         # If all pass, we retweet.
-        try:
-            user_id = correct_tweet['user']['id']
-            if "like" in tweet_lower:
-                self.like(tweet_id)
+        user_id = correct_tweet['user']['id']
+        if "like" in tweet_lower or "fav" in tweet_lower:
+            print "likes or fav"
+            self.like(tweet_id)
+        if "follow" in tweet_lower:
+            print "add friend"
             self.add_friend(user_id)
-            self.retweet(tweet_id)
-            writer.writerow([user_id])
-            f.close()
-            print "finished"
-            time.sleep(random.random() * 50)
-        except BaseException as e:
-            print e
+        self.retweet(tweet_id)
+        print "retweeted"
+        writer.writerow([user_id])
+        f.close()
+        print "finished"
+        time.sleep(random.random() * 50)
 
 if __name__ == "__main__":
     c = LarryBot()
